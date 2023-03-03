0792d0dfaa38b28413eacf1bb38b9b1ce316c01f
bishal-github <shreesbishal44@gmail.com>
2023-03-03 21:30:50 +0545

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Offers from "./components/Offers"
+import Plan from "./components/Plan"
+import Rooms from "./components/Rooms"
+import ImageSlider from "./components/ImageSlider"
+import Footer from "./components/Footer"
+
 
 function App() {
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1464093515883-ec948246accb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2659&q=80',
+      title: 'Lobster',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1512132411229-c30391241dd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Sushi',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Pasta',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1539136788836-5699e78bfc75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Salmon',
+    },
+  ];
 
   return (
     <div className="App">
-      
+      <Navbar />
+      <Hero />
+      <Offers />
+      <Plan />
+      <Rooms />
+      <ImageSlider />
+      <Footer />
     </div>
   )
 }
